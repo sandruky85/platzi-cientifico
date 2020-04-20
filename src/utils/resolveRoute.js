bf450f128f906d97a2e7d053eff14af6c7d3033e
sandra.menendez <sandra.menendez@rakuten.de>
2020-04-20 16:45:31 +0200

    Starting platzi-cientifico project

---
+++ src/utils/resolveRoute.js
@@ -0,0 +1,9 @@
+const resolveRoute = (route) => {
+    if (route.length <= 3) {
+      let validRoute = route === "/" ? route : "/:id";
+      return validRoute;
+    }
+    return `/${route}`;
+}
+
+export default resolveRoute;
